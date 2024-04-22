349d5ac8d0f0ce1ea111bd1fea4349e2fecee113
Vishal Marati <106393542+vhack0679@users.noreply.github.com>
2024-04-22 19:55:10 +0530

    day 2

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $('.gallery-img').click(function() {
+      var src = $(this).attr('data-src');
+      $('#previewImage').attr('src', src);
+    });
+  });
+function isInViewport(element) {
+var rect = element.getBoundingClientRect();
+return (
+  rect.top >= 0 &&
+  rect.left >= 0 &&
+  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+);
+}
+function fadeInOnScroll() {
+var elements = document.querySelectorAll('.fade-in');
+elements.forEach(function(element) {
+  if (isInViewport(element)) {
+      element.classList.add('fade-in-show');
+  } else {
+      element.classList.remove('fade-in-show');
+  }
+});
+}
+fadeInOnScroll();
+window.addEventListener('scroll', function() {
+fadeInOnScroll();
+});
+window.addEventListener('DOMContentLoaded', function() {
+var myModal = new bootstrap.Modal(document.getElementById('admissionsModal'), {
+keyboard: false
+});
+myModal.show();
+});
+function scrollToTop() {
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  }
+  window.addEventListener('scroll', function() {
+      var scrollButton = document.querySelector('.scroll-to-top');
+      if (window.scrollY > 300) {
+          scrollButton.style.display = 'flex'; 
+      } else {
+          scrollButton.style.display = 'none'; 
+      }
+  });
